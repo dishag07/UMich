86c37c400686e6906a6b770218516184c205f485
dishag07 <dishag422@gmail.com>
2024-09-16 10:40:39 -0400

    pictures need to be fixed // end of day tew

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 const quotes = [
-    "'Top to bottom Michigan is about excellence, greatness. You have my pledge I will carry forward the excellence of Michigan football",
+    "'Top to bottom Michigan is about excellence, greatness. You have my pledge I will carry forward the excellence of Michigan football'",
     "'DHSP is a great place to build on your math skills, and make amazing connections in a supportive environment surrounded by people who are all very similar to you and I'd recommend it to anyone'",
     "'You can't fail there. You have so much support. So much help. We have the academic center. We can get tutors when we want and the professors are good'",
 ];
